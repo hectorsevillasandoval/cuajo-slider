bd5bd392c3323af4600d810b6eec963d38dd6b20
hsevilla <sevillasandoval.hector@gmail.com>
2021-03-15 13:32:15 -0600

    Adding prototype v

--- cuajo-slider.js
+++ cuajo-slider.js
@@ -1,51 +1,56 @@
 function Slider(slider) {
     if (!(slider instanceof Element)) throw new Error('No slider passed in');
-    // Create some variables for working with the slider
-    let current, prev, next;
+
+
     // Select elements needed for the slide
-    const slides = slider.querySelector('.slides'),
-        prevButton = slider.querySelector('.goToPrev'),
-        nextButton = slider.querySelector('.goToNext');
+    this.slider = slider;
+    this.slides = slider.querySelector('.slides');
+    const prevButton = slider.querySelector('.goToPrev');
+    const nextButton = slider.querySelector('.goToNext');
 
-    // Function to Start the slider
-    function startSlider() {
-        current = slides.querySelector('.current') || slides.firstElementChild;
-        prev = current.previousElementSibling || slides.lastElementChild;
-        next = current.nextElementSibling || slides.firstElementChild;
-        console.log(current, prev, next);
-    }
 
-    // Add classes to elements
-    function addClasses() {
-        current.classList.add('current');
-        prev.classList.add('prev');
-        next.classList.add('next');
-    }
-
-    // Move classes
-    function move(direction) {
-        // Create array of classes to remove
-        const classesToRemove = ['prev', 'current', 'next'],
-            elementsWhereToRemove = [prev, current, next];
-
-        elementsWhereToRemove.forEach(element => element.classList.remove(...classesToRemove));
-
-        if (direction === 'back') {
-            [prev, current, next] = [prev.previousElementSibling || slides.lastElementChild, prev, current];
-        } else {
-            [prev, current, next] = [current, next, next.nextElementSibling || slides.firstElementChild];
-        }
-
-        addClasses();
-
-    }
-    startSlider();
-    addClasses();
+    this.startSlider();
+    this.addClasses();
 
     // Event Listeners
-    prevButton.addEventListener('click', () => move('back'));
-    nextButton.addEventListener('click', () => move('nacatamal'));
+    prevButton.addEventListener('click', () => this.move('back'));
+    nextButton.addEventListener('click', () => this.move('nacatamal'));
 }
 
-const mySlider = Slider(document.querySelector('.slider')),
-    dogSlider = Slider(document.querySelector('.dog-slider'));
+// Function to Start the slider
+Slider.prototype.startSlider = function() {
+    this.current = this.slides.querySelector('.current') || this.slides.firstElementChild;
+    this.prev = this.current.previousElementSibling || this.slides.lastElementChild;
+    this.next = this.current.nextElementSibling || this.slides.firstElementChild;
+    console.log(this.current, this.prev, this.next);
+}
+
+// Add classes to elements
+Slider.prototype.addClasses = function() {
+    this.current.classList.add('current');
+    this.prev.classList.add('prev');
+    this.next.classList.add('next');
+}
+
+// Move classes
+Slider.prototype.move = function(direction) {
+    // Create array of classes to remove
+    const classesToRemove = ['prev', 'current', 'next'],
+        elementsWhereToRemove = [this.prev, this.current, this.next];
+
+    elementsWhereToRemove.forEach(element => element.classList.remove(...classesToRemove));
+
+    if (direction === 'back') {
+        [this.prev, this.current, this.next] = [this.prev.previousElementSibling || this.slides.lastElementChild, this.prev, this.current];
+    } else {
+        [this.prev, this.current, this.next] = [this.current, this.next, this.next.nextElementSibling || this.slides.firstElementChild];
+    }
+
+    this.addClasses();
+
+}
+
+const mySlider = new Slider(document.querySelector('.slider')),
+    dogSlider = new Slider(document.querySelector('.dog-slider'));
+
+console.log(mySlider, dogSlider);
